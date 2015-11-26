cc3e8cc6918775aff3ac6d7cb24c20828eb1374f
MikaelBergquist <bergquist.mikael@gmail.com>
2015-11-26 01:31:04 +0100

    Första riktiga comitten, en bas med två bakgrunder med två kort.

--- app/src/main/java/com/watch/iot/iotcc/MainActivity.java
+++ app/src/main/java/com/watch/iot/iotcc/MainActivity.java
@@ -1,24 +1,45 @@
 package com.watch.iot.iotcc;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+        import android.app.Activity;
+        import android.content.res.Resources;
+        import android.os.Bundle;
+        import android.support.wearable.view.DotsPageIndicator;
+        import android.support.wearable.view.GridViewPager;
+        import android.view.View;
+        import android.view.View.OnApplyWindowInsetsListener;
+        import android.view.WindowInsets;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+        final Resources res = getResources();
+        final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
+        pager.setOnApplyWindowInsetsListener(new OnApplyWindowInsetsListener() {
             @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                // Adjust page margins:
+                //   A little extra horizontal spacing between pages looks a bit
+                //   less crowded on a round display.
+                final boolean round = insets.isRound();
+                int rowMargin = 0;//res.getDimensionPixelOffset(R.dimen.page_row_margin);
+                int colMargin = 0;//res.getDimensionPixelOffset(round ?
+                       // R.dimen.page_column_margin_round : R.dimen.page_column_margin);
+                pager.setPageMargins(rowMargin, colMargin);
+
+                // GridViewPager relies on insets to properly handle
+                // layout for round displays. They must be explicitly
+                // applied since this listener has taken them over.
+                pager.onApplyWindowInsets(insets);
+                return insets;
             }
         });
+
+        pager.setAdapter(new IoTGridPagerAdapter(this, getFragmentManager()));
+        DotsPageIndicator dotsPageIndicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
+        dotsPageIndicator.setPager(pager);
     }
 }
+
